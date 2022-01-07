c766c83c1b6297e4a6ec94025eacf758d6f3d1d7
Mara <maraaazahan@gmail.com>
2022-01-08 00:55:53 +0200

    Add RateTripActivity

--- app/src/main/java/com/example/travelassistant/TripsFragment.kt
+++ app/src/main/java/com/example/travelassistant/TripsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.travelassistant
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,13 @@ class TripsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trips, container, false)
+        val view = inflater.inflate(R.layout.fragment_trips, container, false)
+        val parisImageView = view.findViewById<ImageView>(R.id.parisImageView)
+        parisImageView.setOnClickListener({
+            val intent = Intent(activity, RateTripActivity::class.java)
+            startActivity(intent)
+        })
+        return view
     }
 
     companion object {
